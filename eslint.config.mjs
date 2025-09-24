2aefe327c29cbbbaf804ae363fdb1e6c4f06cf91
Sayan <dassayan004@gmail.com>
2025-09-24 13:30:29 +0530

    lint fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,16 +10,19 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    ignores: [
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript"],
+    ignorePatterns: [
       "node_modules/**",
       ".next/**",
       "out/**",
       "build/**",
       "next-env.d.ts",
     ],
-  },
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  }),
 ];
 
 export default eslintConfig;
